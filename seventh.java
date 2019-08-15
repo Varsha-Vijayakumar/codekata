2c208db9469126ec14d9a59941f6b55b5b65a9dd
varsha <varsha.vijayakumar07@gmail.com>
2019-08-16 00:23:57 +0530

    set1-7

---
+++ seventh.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class seventh
+{
+	public static void main(String []args)
+	{
+		int n,i;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(i=0;i<n;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
+		
