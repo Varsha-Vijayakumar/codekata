074bfdfdfa05d6511aac020f576b19a8427c3b02
varsha <varsha.vijayakumar07@gmail.com>
2019-08-16 00:31:29 +0530

    set1-8

---
+++ eigth.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class eigth
+{
+	public static void main(String []args)
+	{
+		int n,i,sum=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
+		
