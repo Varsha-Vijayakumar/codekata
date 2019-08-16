294794863f96ea4512a7c218ead5f17d1f55d330
varsha <varsha.vijayakumar07@gmail.com>
2019-08-16 15:21:48 +0530

    set2-1

---
+++ eleven.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class eleven
+{
+	public static void main(String[] args)
+	{
+		int n,k;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		k=s.nextInt();
+		System.out.println(Math.pow(n,k));
+	}
+}
