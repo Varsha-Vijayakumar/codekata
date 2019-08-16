e05e0c4f21d6b6ea964e4f894715be2cce651aec
varsha <varsha.vijayakumar07@gmail.com>
2019-08-16 19:45:03 +0530

    set2-4

---
+++ fourteen.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class fourteen
+{
+	public static void main(String[] args)
+	{
+		int n1,n2,i;
+		Scanner s=new Scanner(System.in);
+		n1=s.nextInt();
+		n2=s.nextInt();
+		n1++;
+		while(n1<n2)
+		{
+			if(n1%2==0)
+			{
+			}
+			else
+			{
+				System.out.println(n1);
+			}
+		n1++;
+		}
+	}
+}
