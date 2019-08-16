aec537b01a421b6419f13667a183b43d739f2036
varsha <varsha.vijayakumar07@gmail.com>
2019-08-16 20:02:42 +0530

    set2-5

---
+++ fifteen.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class fifteen
+{
+	public static void main(String[] args)
+	{
+		int n1,n2,i;
+		Scanner s=new Scanner(System.in);
+		n1=s.nextInt();
+		n2=s.nextInt();
+		n1++;
+		while(n1<n2)
+		{
+			if(n1%2==0)
+			{
+				System.out.println(n1);
+			}
+			else
+			{
+			}
+		n1++;
+		}
+	}
+}
